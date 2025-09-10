5ad6a9d3a224c8c077f2c6cdec1342969ded76a5
Koustav <koustavganguly24@gmail.com>
2025-09-10 17:26:00 +0530

    added future plan in areaofimprovement2.md file

--- .next/server/app/icon/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../chunks/[turbopack]_runtime.js")("server/app/icon/route.js")
-R.c("server/chunks/[externals]_next_dist_compiled_@vercel_og_index_node_f9472a75.js")
-R.c("server/chunks/d4a42_next_4abafdaa._.js")
-R.c("server/chunks/[root-of-the-server]__804ce9df._.js")
-R.m("[project]/.next-internal/server/app/icon/route/actions.js [app-rsc] (server actions loader, ecmascript)")
-R.m("[project]/node_modules/.pnpm/next@15.5.0_react-dom@19.1.1_react@19.1.1__react@19.1.1/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/icon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)")
-module.exports=R.m("[project]/node_modules/.pnpm/next@15.5.0_react-dom@19.1.1_react@19.1.1__react@19.1.1/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/icon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)").exports
