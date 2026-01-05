0488ee5fd027ac6648e4b5efb1a47835e96d4d52
KOUSTAV <koustavganguly24@gmail.com>
2026-01-05 22:42:24 +0530

    updated experience section

--- src/components/WorkExperienceSection.tsx
+++ src/components/WorkExperienceSection.tsx
@@ -47,28 +47,16 @@ export default function WorkExperienceSection() {
               {work.link && (
                 <div className="mt-3">
                   <Link 
-                    className="block border border-gray-200 rounded-lg hover:shadow-2xs focus:outline-hidden dark:border-neutral-700" 
+                    className="block border border-gray-200 rounded-lg hover:shadow-2xs focus:outline-hidden dark:border-neutral-700 p-4" 
                     href={work.link}
                   >
-                    <div className="relative flex items-center overflow-hidden">
-                      <Image 
-                        className="w-32 sm:w-48 h-full absolute inset-0 object-cover rounded-s-lg" 
-                        src="https://images.unsplash.com/photo-1661956600655-e772b2b97db4?q=80&w=560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-                        alt={`${work.company} project`}
-                        width={192}
-                        height={96}
-                      />
-
-                      <div className="grow p-4 ms-32 sm:ms-48">
-                        <div className="min-h-24 flex flex-col justify-center">
-                          <h3 className="font-semibold text-sm text-gray-800 dark:text-neutral-300">
-                            {work.company}
-                          </h3>
-                          <p className="mt-1 text-sm text-gray-500 dark:text-neutral-500">
-                            {work.location} • {work.employmentType}
-                          </p>
-                        </div>
-                      </div>
+                    <div className="flex flex-col justify-center">
+                      <h3 className="font-semibold text-sm text-gray-800 dark:text-neutral-300">
+                        {work.company}
+                      </h3>
+                      <p className="mt-1 text-sm text-gray-500 dark:text-neutral-500">
+                        {work.location} • {work.employmentType}
+                      </p>
                     </div>
                   </Link>
                 </div>
